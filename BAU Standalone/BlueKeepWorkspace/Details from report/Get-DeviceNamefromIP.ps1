b6d010fb193466f9f75ef32cdb8f1141a98f6066
alexb3k <alex.andrei3108@outlook.com>
2026-06-19 10:10:30 +0100

    Delete BAU Standalone/BlueKeepWorkspace/Details from report directory

--- BAU Standalone/BlueKeepWorkspace/Details from report/Get-DeviceNamefromIP.ps1
+++
@@ -1,7 +0,0 @@
-$list = gc "C:\Users\RO10029\OneDrive - SIG\zzz.CODE\BlueKeepWorkspace\Details from report\VulnerableIPList.txt"
-
-foreach ($ip in $list) 
-{   
-    ping -a $ip >> "C:\Users\RO10029\OneDrive - SIG\zzz.CODE\BlueKeepWorkspace\Details from report\Names.txt"
-}
-
